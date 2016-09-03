2b5d191a883bd400f5255b686afa14613329516b
rafiparvez <rafiparvez@gmail.com>
2016-09-03 18:59:41 -0500

    Changes for week 6 assignment

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -50,17 +50,15 @@ error_val   = zeros(m, 1);
 %           
 %       end
 %
-
 % ---------------------- Sample Solution ----------------------
-
-
-
-
-
-
-
+for i = 1:m
+    [theta] = trainLinearReg([ones(i, 1) X(1:i,:)], y(1:i,:), lambda);
+    error_train(i)=linearRegCostFunction([ones(i,1) X(1:i,:)] , y(1:i,:),theta, 0);
+    error_val(i)=linearRegCostFunction([ones(size(Xval,1),1) Xval],yval,theta, 0);
+end
 % -------------------------------------------------------------
-
+error_train=error_train(:);
+error_val=error_val(:);
 % =========================================================================
 
 end
--- machine-learning-ex5/ex5/validationCurve.m
+++ machine-learning-ex5/ex5/validationCurve.m
@@ -38,16 +38,14 @@ error_val = zeros(length(lambda_vec), 1);
 %       end
 %
 %
-
-
-
-
-
-
-
-
-
-
+m=size(X,1);
+X=[ones(m,1) X];
+Xval=[ones(size(Xval,1),1) Xval]
+for i = 1:length(lambda_vec)
+    lambda = lambda_vec(i);
+    [theta] = trainLinearReg(X, y, lambda);
+    error_train(i)=linearRegCostFunction(X,y,theta, 0);
+    error_val(i)=linearRegCostFunction(Xval,yval,theta, 0);
 % =========================================================================
 
 end
